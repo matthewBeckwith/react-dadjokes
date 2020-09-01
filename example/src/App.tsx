d1edf6009a727261502b36ef5972be85fb4298c2
Matthew Beckwith <kodahscripts@outlook.com>
2020-09-01 14:01:26 -0400

    fixing package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-dadjokes'
+import { DadJokesComponent } from 'react-dadjokes'
 import 'react-dadjokes/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <DadJokesComponent />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
-}
+const API = 'https://icanhazdadjoke.com/'
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const DadJokesComponent = () => {
+  const [id, setId] = React.useState(null)
+  const [joke, setJoke] = React.useState(null)
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    fetch(API, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data: ', data)
+        setId(data.id)
+        setJoke(data.joke)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        console.log('Error: ', err)
+      })
+  }, [isLoading])
+
+  if (!isLoading) {
+    return (
+      <div className={styles.test} key={id}>
+        {joke}
+      </div>
+    )
+  } else {
+    return <div className={styles.test}>Loading...</div>
+  }
 }
